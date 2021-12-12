b5933212f99e86a0a7edabcf26707d6dfb7d61fa
Gert Van Gool <gertvangool@gmail.com>
2021-12-11 19:40:31 -0800

    Solving day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 
 enum SyntaxError {
     Corrupted(char),
-    Incomplete,
+    Incomplete(usize),
 }
 
 lazy_static! {
@@ -28,6 +28,16 @@ fn syntax_error_score(c: char) -> usize {
     }
 }
 
+fn syntax_incomplete_score(c: char) -> usize {
+    match c {
+        ')' => 1,
+        ']' => 2,
+        '}' => 3,
+        '>' => 4,
+        _ => panic!(),
+    }
+}
+
 fn check_syntax(line: &str) -> Result<(), SyntaxError> {
     let mut stack: Vec<char> = Vec::new();
     for c in line.chars() {
@@ -40,6 +50,15 @@ fn check_syntax(line: &str) -> Result<(), SyntaxError> {
                 return Err(SyntaxError::Corrupted(c));
             }
         }
+    }
+    if stack.len() > 0 {
+        let mut score: usize = 0;
+        while stack.len() > 0 {
+            let last = stack.pop().unwrap();
+            let endc = CHAR_MAP[&last];
+            score = score * 5 + syntax_incomplete_score(endc);
+        }
+        return Err(SyntaxError::Incomplete(score));
     }
     Ok(())
 }
@@ -56,4 +75,19 @@ pub fn solve_part1(input: &str) -> usize {
         }
     }
     score
+}
+
+#[aoc(day10, part2)]
+pub fn solve_part2(input: &str) -> usize {
+    let mut all_scores: Vec<usize> = Vec::new();
+    for line in input.lines() {
+        match check_syntax(line) {
+            Err(SyntaxError::Incomplete(score)) => {
+                all_scores.push(score);
+            }
+            _ => continue,
+        }
+    }
+    all_scores.sort();
+    all_scores[all_scores.len() / 2]
 }
